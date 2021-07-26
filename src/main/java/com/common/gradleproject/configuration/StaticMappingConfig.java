0de2bd5a63646ec2ea5d3411e56280dcf15642ac
JIHYEON <jenni@paykhan.org>
2021-07-26 17:54:35 +0900

    commit

---
+++ src/main/java/com/common/gradleproject/configuration/StaticMappingConfig.java
@@ -0,0 +1,16 @@
+package com.common.gradleproject.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class StaticMappingConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
+		registry.addResourceHandler("/lib/**").addResourceLocations("classpath:/lib/");
+	}
+
+}
